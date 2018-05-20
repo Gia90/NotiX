22146cd39a79afc73bede94c57444b1d6978e02f
Gia90 <Gia90@users.noreply.github.com>
2018-05-20 23:45:41 +0200

    Better hook

--- app/src/main/java/org/dev/g14/notix/NotixHook.java
+++ app/src/main/java/org/dev/g14/notix/NotixHook.java
@@ -2,74 +2,68 @@ package org.dev.g14.notix;
 
 import android.app.Notification;
 import android.content.Context;
-import android.os.UserHandle;
+import android.service.notification.NotificationListenerService.RankingMap;
+import android.service.notification.StatusBarNotification;
 
-import de.robv.android.xposed.IXposedHookZygoteInit;
+import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
-import static de.robv.android.xposed.XposedHelpers.getObjectField;
+import static de.robv.android.xposed.XposedHelpers.findClass;
 
 /**
  * Created by gia on 21/01/18.
  */
 
-public class NotixHook implements IXposedHookZygoteInit {
+public class NotixHook implements IXposedHookLoadPackage {
 
-    private boolean killNotification(Notification n, Context nContext)
+    private boolean isToBeKilled(Notification n, String pkgName)
     {
-        String nPackageName = nContext.getPackageName();
         String nTitle = n.extras.getString(Notification.EXTRA_TITLE);
         String nText = n.extras.getString(Notification.EXTRA_TEXT);
 
-        XposedBridge.log("   from " + nPackageName);
+        XposedBridge.log("   from " + pkgName);
         XposedBridge.log("   " + nTitle + ": " + nText);
         // Signal?? https://github.com/WhisperSystems/Signal-Android/blob/e7a9893e94659779680cedcfc3398a664e12abad/src/org/thoughtcrime/securesms/service/MessageRetrievalService.java
 
-        if( nPackageName.equals("org.thoughtcrime.securesms") && nText.equals("Background connection enabled") ) {
-            XposedBridge.log("   KILL IT!");
+        // TODO: check user defined filters to decide whether to kill this notification or not
+        if( pkgName.equals("org.thoughtcrime.securesms") && nText.equals("Background connection enabled") ) {
             return true;
         }
         return false;
 
     }
+
     @Override
-    public void initZygote(StartupParam startupParam) throws Throwable {
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
         /** https://github.com/aosp-mirror/platform_frameworks_base/tree/master/core */
+        if (!lpparam.packageName.equals("com.android.systemui")) return;
 
-        // https://github.com/aosp-mirror/platform_frameworks_base/blob/master/core/java/android/app/NotificationManager.java#L295
-        // Hooking method: public void notifyAsUser(String tag, int id, Notification notification, UserHandle user)
-        findAndHookMethod("android.app.NotificationManager", null, "notifyAsUser", String.class, int.class, Notification.class, UserHandle.class, new XC_MethodHook() {
+        final Class<?> phoneStatusBarClass = findClass("com.android.systemui.statusbar.phone.PhoneStatusBar", lpparam.classLoader);
+
+        // https://github.com/aosp-mirror/platform_frameworks_base/blob/nougat-release/packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+        // Hooking method: public void addNotification(StatusBarNotification notification, RankingMap ranking, Entry oldEntry)
+        findAndHookMethod(phoneStatusBarClass, "addNotification", StatusBarNotification.class, RankingMap.class, "com.android.systemui.statusbar.NotificationData$Entry", new XC_MethodHook() {
             @Override
-            protected void beforeHookedMethod(MethodHookParam methodHookParam) throws Throwable {
-                Context nContext = (Context)getObjectField(methodHookParam.thisObject, "mContext");
-                String nTag = (String)methodHookParam.args[0];
-                int nId = (int)methodHookParam.args[1];
-                Notification n = (Notification)methodHookParam.args[2];
-                UserHandle nUserHandle = (UserHandle)methodHookParam.args[3];
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                final StatusBarNotification notif = (StatusBarNotification) param.args[0];
+                final String pkg = notif.getPackageName();
+                final boolean clearable = notif.isClearable();
+                final int id = notif.getId();
+                final Notification n = notif.getNotification();
 
-                XposedBridge.log("  [New notification] notifyAsUser!");
-                killNotification(n, nContext);
-            }
-        });
+                // Make any notification clearable (WHY IT DOESN'T WORK??)
+                n.flags &= ~Notification.FLAG_FOREGROUND_SERVICE;
 
-        // https://github.com/aosp-mirror/platform_frameworks_base/blob/master/core/java/android/app/Service.java#L697
-        // Hooking method: public final void startForeground(int id, Notification notification)
-        findAndHookMethod("android.app.Service", null, "startForeground", int.class, Notification.class, new XC_MethodReplacement() {
-            @Override
-            protected Object replaceHookedMethod(MethodHookParam methodHookParam) throws Throwable {
-                Context nContext = (Context)methodHookParam.thisObject;
-                int nId = (int)methodHookParam.args[0];
-                Notification n = (Notification)methodHookParam.args[1];
+                //n.extras.putString(Notification.EXTRA_TITLE, "NOTIX: "+n.extras.getString(Notification.EXTRA_TITLE) );
 
-                XposedBridge.log("  [New notification] startForeground!");
-                killNotification(n, nContext);
-
-                XposedBridge.invokeOriginalMethod(methodHookParam.method, methodHookParam.thisObject, methodHookParam.args);
-
-                return null;
+                // to kill or not to kill?
+                if( isToBeKilled(n, pkg) ) {
+                    XposedBridge.log("   KILL IT!");
+                    param.setResult(null);
+                }
             }
         });
     }
